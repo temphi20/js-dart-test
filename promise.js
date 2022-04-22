8bb8f17e0dc59a1ffc6ffef5e779b3c5690ca1b5
Joohyun Jo <jh.jo@wgslabs.com>
2022-04-22 16:19:14 +0900

    promise test

---
+++ promise.js
@@ -0,0 +1,45 @@
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
+}
+
+async function test(val){
+    console.log(`${val} - start`);
+    await delay(2);
+    console.log(`${val} - end`);
+    return true;
+}
+
+async function promiseAll(){
+    const arr = ['qwer', 'asdf', 'xzcv', '1234', '5678', '90-='];
+    console.time('promise.all array set');
+    const arrP = Array.from(arr, x => test(x)); 
+    console.timeEnd('promise.all array set');
+
+    console.time('promise.all');
+    await Promise.all(arrP);
+    console.timeEnd('promise.all');
+}
+async function promise(){
+    console.time('promise');
+    await test('1');
+    await test('2');
+    await test('3');
+    await test('4');
+    await test('5');
+    console.timeEnd('promise');
+}
+
+// console.time('main');
+// main()
+// console.timeEnd('main');
+// await test('test')
+// await test('1111')
+// console.time('promiseTest');
+// promiseTest();
+// console.timeEnd('promiseTest');
+
+
+promiseAll();
+promise();
